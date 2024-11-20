ffea444e9a3c4ab353d413c242586bb0d75a97d4
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2024-11-20 22:52:43 +0530

    worked on home page : product details

--- auroma_aura/src/Home.js
+++ auroma_aura/src/Home.js
@@ -8,39 +8,50 @@ function Home() {
             <div className="home-image">
                 <img className="image" src="Images/earth.jpg" alt="show images"></img>
             </div>
-
-
-            
                 <div className="rows">
                     <Product 
+                    id="12345"
                     productname="Product-1" 
                     price={20} 
                     image ={<img src='Images/herbal.png' alt="product-image"></img>} 
                     rating={5}/>
                     <Product 
+                    id="12345"
                     productname="Product-2" 
                     price={20} 
                     image ={<img src='Images/Sambrani.png' alt="product-image"></img>} 
-                    rating={5}/>
+                    rating={3}/>
                     <Product 
+                    id="12345"
                     productname="Product-3" 
                     price={20} 
                     image ={<img src='Images/Itachi.jpg' alt="product-image"></img>} 
-                    rating={5}/>
-                    
-                    
-
+                    rating={4}/>
                 </div>
 
                 <div className="rows">
-                    <Product />
-                    <Product />
+                <Product 
+                    id="12345"
+                    productname="Product-1" 
+                    price={20} 
+                    image ={<img src='Images/herbal.png' alt="product-image"></img>} 
+                    rating={2}/>
+                    <Product 
+                    id="12345"
+                    productname="Product-3" 
+                    price={20} 
+                    image ={<img src='Images/Itachi.jpg' alt="product-image"></img>} 
+                    rating={3}/>
                 </div>
+                
                 <div className="rows">
-                    <Product />
-                </div>
-            
-            
+                <Product 
+                    id="12345"
+                    productname="Product-2" 
+                    price={20} 
+                    image ={<img src='Images/Sambrani.png' alt="product-image"></img>} 
+                    rating={3}/>
+                </div>            
         </div>
 
 
